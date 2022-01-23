a339f9cc958f04501df04120dd6669f406ceedab
mattmuroya <matt.muroya@gmail.com>
2022-01-23 03:32:49 -1000

    add event listeners for spaces

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // draw the grid and assign ids
+// add event listener for each space
 
 // |0|1|2|
 // |3|4|5|
 // |6|7|8|
 
-(function() {
-  let board = document.querySelector('.board');
+const gameBoard = (() => {
+
+  // create the board array
+  const board = [
+    '', '', '',
+    '', '', '',
+    '', '', '',];
+  
+  // draw the board, assign ids, add event listeners
+  let grid = document.querySelector('.board');
   for (let i = 0; i < 9; i++) {
       let space = document.createElement('div');
+      grid.appendChild(space);
       space.id = i;
       space.className = 'space';
-      board.appendChild(space);
+      space.addEventListener('click', (e) => {
+        update(e.target.id, game.getActivePlayer().character);
+        game.changeActivePlayer();
+      })
   }
-})();
 
-// game board object 
-
-const gameBoard = (() => {
-  const board = [
-      '', '', '',
-      '', '', '',
-      '', '', '',];
   const update = (spaceId, playerChar) => {
     board[spaceId] = playerChar;
-    document.getElementById('4')
+    document.getElementById(spaceId)
         .insertAdjacentHTML('afterbegin', `<p>${playerChar}</p>`);
   };
-  return{update};
+
+  return{board};
 })();
 
 // player factory
@@ -38,7 +44,17 @@ const Player = (name, character) => {
   const logCharacter = () => {
     console.log(character);
   };
-  return {logName, logCharacter};
+  return {name, character, logName, logCharacter};
 };
 
 const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+
+// game object
+
+const game = ((p1, p2) => {
+  let activePlayer = p1;
+  const getActivePlayer = () => activePlayer;
+  const changeActivePlayer = () => activePlayer = activePlayer === p1 ? p2 : p1;
+  return {getActivePlayer, changeActivePlayer};
+})(player1, player2);
